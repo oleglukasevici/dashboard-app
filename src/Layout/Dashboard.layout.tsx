d41c8ad691dffa120158bed32f50d98a2d0ca469
oleglukasevici <ardorfire0@gmail.com>
2023-09-10 20:33:31 +0300

    feat: tailwind css lib add

---
+++ src/Layout/Dashboard.layout.tsx
@@ -0,0 +1,8 @@
+const DashboardLayout = () => {
+  return (
+    <div>
+      <span className="text-3xl font-bold underline">Hello world!</span>
+    </div>
+  );
+};
+export default DashboardLayout;
