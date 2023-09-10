e79ff32cc1266d27a08acb14be0977991f156cd9
oleglukasevici <ardorfire0@gmail.com>
2023-09-10 22:46:35 +0300

    feat: Login page created

--- src/bootstrap/RouterProvider/router.tsx
+++ src/bootstrap/RouterProvider/router.tsx
@@ -1,7 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import OverviewPage from "../../containers/Overview";
+import LoginPage from "../../containers/Login";
 
 export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
   {
     path: "/overview",
     element: <OverviewPage />,
---
+++ src/containers/Login/index.tsx
@@ -0,0 +1,47 @@
+import { AppSettings } from "../../settings/App.settings";
+
+const LoginPage: React.FunctionComponent = () => {
+  return (
+    <div className="flex items-center justify-center h-screen flex-col ">
+      <div className="text-3xl p-10">{AppSettings.name}</div>
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h1 className="text-2xl font-semibold mb-4">Login</h1>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              className="w-full p-2 border rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              className="w-full p-2 border rounded"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default LoginPage;
